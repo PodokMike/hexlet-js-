e6f41a96452bdac974e85c22aa72252c69fba9f1
Mikhail Dolgov <podok2@gmail.com>
2025-03-29 11:17:25 +0300

    add src

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// import _ from "lodash";
+// //console.log('Hello Mike');
+// console.log(_.last(["one", "two"]));
+
+// file: src/index.js
+export default (age) => {
+    const birthYear = 2005; // Подсчитываем результат
+  
+    return birthYear;
+  };
+  
+  // file: index.js
+  import getBirthYear from './src/index.js';
+  
+  console.log('Сколько вам лет?');
+  const age = 18; // Каким-то способом получаем возраст пользователя, через терминал. Сейчас не важно как
+  const birthYear = getBirthYear(age);
+  console.log(`Ваш год рождения: ${birthYear}`);
